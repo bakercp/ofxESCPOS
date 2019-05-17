8ea61f40f0952e9ab16fee2dfc467dbbf8159ba6
Christopher Baker <info@christopherbaker.net>
2019-05-17 13:43:11 -0600

    In progress.

--- libs/ofxESCPOS/src/DefaultSerialPrinter.cpp
+++ libs/ofxESCPOS/src/DefaultSerialPrinter.cpp
@@ -24,17 +24,23 @@ DefaultSerialPrinter::~DefaultSerialPrinter()
 
 std::string DefaultSerialPrinter::getSerialNumber()
 {
-//    const uint8_t command[3] = {
-//        ESCPOS::BaseCodes::GS,
-//        'I',
-//        68
-//    };
-//
-//    std::size_t b = writeBytes(command, 3);
-//
-//    char buffer[256];
+    const uint8_t command[3] = {
+        ESCPOS::BaseCodes::GS,
+        'I',
+        68
+    };
 
-//    std::size bytesRead = read(buffer, 256);
+    std::size_t b = writeBytes(command, 3);
+
+    std::cout << b << " bytes written." << std::endl;
+
+    uint8_t buffer[256];
+
+    std::size_t bytesRead = readBytes(buffer, 256);
+
+    std::cout << bytesRead << " bytes read." << std::endl;
+
+    return "";
 }
 
 
