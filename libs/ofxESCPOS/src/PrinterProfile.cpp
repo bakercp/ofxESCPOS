8096da60bbd715241d50664006423de083c734cf
Christopher Baker <me@christopherbaker.net>
2019-05-30 14:59:47 -0600

    Update default code pages based on what is supported by iconv.

--- libs/ofxESCPOS/src/PrinterProfile.cpp
+++ libs/ofxESCPOS/src/PrinterProfile.cpp
@@ -15,13 +15,13 @@ namespace ESCPOS {
 const std::vector<Codes::CodePage> PrinterProfile::DEFAULT_CODE_PAGES =
 {
     Codes::PC437,
-    Codes::Katakana,
+    // Codes::Katakana, // Not supporting this yet, because the code page isn't correct.
     Codes::PC850,
     Codes::PC860,
     Codes::PC863,
     Codes::PC865,
 
-    Codes::PC851,
+    // Codes::PC851, Not used. Inconsistently supported.
     Codes::PC853,
     Codes::PC857,
     Codes::PC737,
@@ -30,14 +30,14 @@ const std::vector<Codes::CodePage> PrinterProfile::DEFAULT_CODE_PAGES =
     Codes::PC866,
     Codes::PC852,
     Codes::PC858,
-    Codes::KU42,
+    // Codes::KU42, Not supporting b/c no iconv support.
     Codes::TIS11,
 
-    Codes::TIS18,
+    // Codes::TIS18, Not supporting b/c no iconv support.
 
-    Codes::TCVN_3_I,
-    Codes::TCVN_3_II,
-    Codes::PC720,
+    // Codes::TCVN_3_I, Not supporting b/c no iconv support.
+    // Codes::TCVN_3_II, Not supporting b/c no iconv support.
+    // Codes::PC720, Not supporting b/c no iconv support.
     Codes::PC775,
     Codes::PC855,
     Codes::PC861,
@@ -46,9 +46,9 @@ const std::vector<Codes::CodePage> PrinterProfile::DEFAULT_CODE_PAGES =
     Codes::PC869,
     Codes::ISO8859_2,
     Codes::ISO8859_15,
-    Codes::PC1098,
-    Codes::PC1118,
-    Codes::PC1119,
+    // Codes::PC1098, Not supporting b/c no iconv support.
+    // Codes::PC1118, Not supporting b/c no iconv support.
+    // Codes::PC1119, Not supporting b/c no iconv support.
     Codes::PC1125,
     Codes::WPC1250,
     Codes::WPC1251,
@@ -57,10 +57,10 @@ const std::vector<Codes::CodePage> PrinterProfile::DEFAULT_CODE_PAGES =
     Codes::WPC1255,
     Codes::WPC1256,
     Codes::WPC1257,
-    Codes::WPC1258,
+    // Codes::WPC1258, Not supporting this yet.
     Codes::KZ_1048,
 
-    Codes::USER_PAGE
+    // Codes::USER_PAGE // Not supporting for now.
 };
 
 
